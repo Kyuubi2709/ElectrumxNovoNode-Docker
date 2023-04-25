fcd5e0575fd0bdbd7dadbbb104c46d5f3de53056
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-04-25 15:06:16 +0200

    finalizing

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ ! -d /data/novochain ]]; then
+  echo -e "Creating /data/novochain directory..."
   mkdir -p /data/novochain
 fi
-
-if [[ ! -d /data/ ]]; then
-  mkdir -p /data/novochain
+if [[ ! -d /data/novo-electrumx-db ]]; then
+  echo -e "Creating /data/novo-electrumx-db directory..."
+  mkdir -p /data/novo-electrumx-db
 fi
-
-
-
 echo -e "Starting daemon and electrumx..."
 bash -c "novod -datadir=/data/novochain -conf=/backend/novo.conf && python3 /backend/novo-electrumx/electrumx_server"
