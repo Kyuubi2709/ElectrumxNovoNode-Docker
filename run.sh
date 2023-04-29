f07152e5e782c09ab55cb762597f1c2e925eece6
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-04-29 09:18:06 +0200

    revert

--- run.sh
+++ run.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ ! -d /root/.novo ]]; then
-echo -e "Creating /data/novochain directory..."
-mkdir -p /root/.novo  
-touch /root/.novo/novo.conf
-cat <<- EOF > /root/.novo/novo.conf
-rpcuser=NovoDockerUser
-rpcpassword=NovoDockerPassword
-listen=1
-daemon=1
-server=1
-rpcworkqueue=512
-rpcthreads=64
-rpcallowip=0.0.0.0/0
-EOF
+if [[ ! -d /data/novochain ]]; then
+  echo -e "Creating /data/novochain directory..."
+  mkdir -p /data/novochain
 fi
-
-if [[ ! -d /root/novo-electrumx-db ]]; then
-  echo -e "Creating /root/novo-electrumx-db directory..."
-  mkdir -p /root/novo-electrumx-db
+if [[ ! -d /data/novo-electrumx-db ]]; then
+  echo -e "Creating /data/novo-electrumx-db directory..."
+  mkdir -p /data/novo-electrumx-db
 fi
-
 echo -e "Starting daemon and electrumx..."
-bash -c "novod && python3 /backend/novo-electrumx/electrumx_server"
+bash -c "novod -datadir=/data/novochain -conf=/backend/novo.conf && python3 /backend/novo-electrumx/electrumx_server"
